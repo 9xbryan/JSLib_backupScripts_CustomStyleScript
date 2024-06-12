dd1fc25b91ed5b24c931bcc2044f3abd1b0c3a30
9xbryan <76468228+9xbryan@users.noreply.github.com>
2024-06-11 20:04:46 -0400

    this version works as intended

--- app.ai.js
+++ app.ai.js
@@ -1,27 +1,37 @@
+/* location:
+https://api.github.com/repos/9xbryan/JSLib_backupScripts_CustomStyleScript/contents/app.ai.js
+https://raw.githubusercontent.com/9xbryan/JSLib_backupScripts_CustomStyleScript/main/app.ai.js
+*/
+
 function categorizeElement(element) {
 		if (element.nodeName === 'IMG') return 'IMG';
-		else return element.nodeName
+		else return "noGrabText"
 	}
 function mouseOverHandler(e) {
-
+	// console.log('mouse over')
+	// document.body.style.backgroundColor = "Navy";
 	const trg = e.target;
-	console.log(trg.outerHTML.match(/<([A-Z][A-Z0-9]+)\b[^>]*>/i)[0])
+	// trg.classList.add('outline');
 	switch (categorizeElement(trg)) {
-		case 'IMG': element.classList.add('outline'); break;
-		case 'noGrabText': element.background.color = 'red';
+		case 'IMG': trg.classList.add('outline'); break;
+		case 'noGrabText': trg.background.color = 'red';
 	}
 
 
 }
 function mouseOutHandler(e) {
+	// console.log('mouse out')
+	// document.body.style.backgroundColor = "red";
 	const trg = e.target;
 	switch (categorizeElement(trg)) {
-		case 'IMG': element.classList.remove('outline'); break;
-		case 'noGrabText': element.background.color = 'revert';
+		case 'IMG': trg.classList.remove('outline'); break;
+		case 'noGrabText': trg.background.color = 'revert';
 	}
-
-
+	// trg.classList.remove('outline');
 }
 
 window.addEventListener('mouseover', mouseOverHandler);
 window.addEventListener('mouseout', mouseOutHandler);
+window.addEventListener('load', e => {
+	console.log('extension loaded')
+});
